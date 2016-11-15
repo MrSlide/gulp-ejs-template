3fe874241d19ed77448461c83649759e5a50f8ab
Luís Rodrigues <netslider25@gmail.com>
2016-11-15 13:55:17 +0000

    Updated the index.js to match other changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 /*
  * gulp-ng-template
  * https://github.com/teambition/gulp-ejs-template
@@ -6,48 +6,48 @@
  * Copyright (c) 2014 Yan Qing
  * Licensed under the MIT license.
  */
-var fs = require('fs');
-var util = require('util');
-var path = require('path');
-var gutil = require('gulp-util');
-var through = require('through2');
-var ejs = require('./lib/ejs');
-var packageName = require('./package.json').name;
-var templatesTpl = fs.readFileSync(path.join(__dirname,'./lib/templates.js'), {encoding: 'utf8'});
+const fs = require('fs')
+const util = require('util')
+const path = require('path')
+const gutil = require('gulp-util')
+const through = require('through2')
+const EJS = require('./lib/ejs')
+const packageName = require('./package.json').name
+const templatesTpl = fs.readFileSync(path.join(__dirname, './lib/templates.js'), {encoding: 'utf8'})
 
-module.exports = function(options) {
-  options = options || {};
+module.exports = function (options) {
+  options = options || {}
 
-  var joinedContent = '';
-  var moduleName = options.moduleName || 'templates';
-  var templates = templatesTpl.replace('moduleName', moduleName);
-  var contentTpl = 'templates[\'%s\']  = templates[\'%s\'] = %s;\n\n';
+  var joinedContent = ''
+  var moduleName = options.moduleName || 'templates'
+  var templates = templatesTpl.replace('moduleName', moduleName)
+  var contentTpl = "templates['%s'] = templates['%s'] = %s;\n\n"
   var joinedFile = new gutil.File({
     cwd: __dirname,
     base: __dirname,
     path: path.join(__dirname, moduleName + '.js')
-  });
+  })
 
-  return through.obj(function(file, encoding, next) {
-    if (file.isNull()) return next();
-    if (file.isStream()) return this.emit('error', new gutil.PluginError(packageName,  'Streaming not supported'));
+  return through.obj(function (file, encoding, next) {
+    if (file.isNull()) return next()
+    if (file.isStream()) return this.emit('error', new gutil.PluginError(packageName, 'Streaming not supported'))
 
-    var name = path.relative(file.base, file.path);
-    var tpl = new ejs(file.contents.toString('utf8'), options.delimiter);
-    joinedContent += util.format(contentTpl, normalName(name), fullName(name), tpl.compile());
-    next();
-  }, function() {
-    joinedContent = joinedContent.trim().replace(/^/gm, '  ');
-    joinedFile.contents = new Buffer(templates.replace('/*PLACEHOLDER*/', joinedContent));
-    this.push(joinedFile);
-    this.push(null);
-  });
+    var name = path.relative(file.base, file.path)
+    var tpl = new EJS(file.contents.toString('utf8'), options.delimiter)
+    joinedContent += util.format(contentTpl, normalName(name), '/' + normalName(name), tpl.compile())
+    next()
+  }, function () {
+    joinedContent = joinedContent.trim().replace(/^/gm, '  ')
+    joinedFile.contents = new Buffer(templates.replace('/* PLACEHOLDER */', joinedContent))
+    this.push(joinedFile)
+    this.push(null)
+  })
 
-  function fullName(name) {
-    return name.replace('\\', '/');
+  function fullName (name) {
+    return name.replace('\\', '/')
   }
 
-  function normalName(name) {
-    return fullName(name).replace(path.extname(name), '');
+  function normalName (name) {
+    return fullName(name).replace(path.extname(name), '')
   }
-};
+}
